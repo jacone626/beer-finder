c2e6decc462fd2c50cbda04b45ce9d687e0fee68
James A Cone <jacone626@gmail.com>
2023-05-02 19:34:42 -0400

    added pairing and activity routes, updated the models

---
+++ controllers/api/activityRoutes.js
@@ -0,0 +1,57 @@
+//get route, get by id route, and create route
+const router = require('express').Router();
+const { Activity } = require('../../models');
+// const withAuth = require("../../utils/auth");
+
+  
+//Get
+router.get("/", async (req, res) => {
+    try {
+    const activityData = await Activity.findAll({
+    });
+    res.status(200).json(activityData);
+    } catch (err) {
+    res.status(500).json(err);
+    }
+});
+
+//Find a specific activity
+router.get("/:id", async (req, res) => {
+    try {
+      const activityData = await Post.findByPk(req.params.id, {
+        include: [{ 
+            model: Activity, 
+            attributes: ["username"] },
+          {
+            model: Comment,
+            include: { 
+                model: User, 
+                attributes: ["username"] }
+          },
+        ],
+      });
+      if (!activityData) {
+        res.status(404).json({ message: "No activity found with that id!" });
+        return;
+      }
+      res.status(200).json(activityData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+//Add a review
+router.post("/", withAuth, async (req, res) => {
+    try {    
+      const review = await Review.create({
+        content: req.body.content,
+        emoji_starRating: req.body.emoji_starRating,
+        pairing_id: req.body.pairing_id,
+        user_id: req.session.user_id,
+      });
+      res.status(200).json(review);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
---
+++ controllers/api/pairingRoutes.js
@@ -0,0 +1,16 @@
+//get route, get by id route, and create route
+const router = require('express').Router();
+const { Pairing } = require('../../models');
+// const withAuth = require("../../utils/auth");
+
+  
+//Get
+router.get("/", async (req, res) => {
+    try {
+    const pairingData = await Pairing.findAll({
+    });
+    res.status(200).json(pairingData);
+    } catch (err) {
+    res.status(500).json(err);
+    }
+});
