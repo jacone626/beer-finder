81870b8fd26fefed74a0a660de494cb1325b715e
James A Cone <jacone626@gmail.com>
2023-05-01 20:11:54 -0400

    added pairing file and updated models to include relationships

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const reviewRoutes = require('./review-routes');
+const cannabisIndexRoutes = require('./cannabisIndexRoutes')
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/reviews', reviewRoutes);
+router.use('/cannabisIndex', cannabisIndexRoutes);
 
 module.exports = router;
