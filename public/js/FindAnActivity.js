7d35d1c0d386f74ab6c9b04082ca5d6923026075
Danial Mahmoudi <danialmahmoudi@danials-air.attlocal.net>
2023-05-07 13:08:02 -0400

    created event listeners for menu bar and strain selections.

--- public/js/FindAnActivity.js
+++ public/js/FindAnActivity.js
@@ -1,67 +1,103 @@
 
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    const activities = [
-        {
-          "id": 1,
-          "name": "Playing guitar"
-        },
-        {
-          "id": 2,
-          "name": "Snowboarding"
-        },
-        {
-          "id": 3,
-          "name": "Volunteering"
-        },
-        {
-          "id": 4,
-          "name": "Photography"
-        },
-        {
-          "id": 5,
-          "name": "Playing chess"
-        },
-        {
-          "id": 6,
-          "name": "Surf fishing"
-        },
-        {
-          "id": 7,
-          "name": "Cooking on a grill"
-        },
-        {
-          "id": 8,
-          "name": "Skydiving"
-        },
-        {
-          "id": 9,
-          "name": "Surfing on the internet"
-        },
-        {
-          "id": 10,
-          "name": "Meditating"
-        },
-        {
-          "id": 11,
-          "name": "Attending a concert"
-        },
-        {
-          "id": 12,
-          "name": "Scuba diving"
-        }
-      ];
-  
-    const randomActivities = activities.sort(() => 0.5 - Math.random()).slice(0, 5);
-  
-    // Generate buttons for each activity
-    randomActivities.forEach(activity => {
-      const button = document.createElement("button");
-      button.classList.add("button", "is-rounded", "is-primary");
-      button.textContent = activity.name;
-      button.addEventListener("click", () => {
-        window.location.replace(`activity/${activity.id}`);
-      });
-      document.getElementById("RandomButtons").appendChild(button);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const activities = [
+    {
+      "id": 1,
+      "name": "Playing guitar"
+    },
+    {
+      "id": 2,
+      "name": "Snowboarding"
+    },
+    {
+      "id": 3,
+      "name": "Volunteering"
+    },
+    {
+      "id": 4,
+      "name": "Photography"
+    },
+    {
+      "id": 5,
+      "name": "Playing chess"
+    },
+    {
+      "id": 6,
+      "name": "Surf fishing"
+    },
+    {
+      "id": 7,
+      "name": "Cooking on a grill"
+    },
+    {
+      "id": 8,
+      "name": "Skydiving"
+    },
+    {
+      "id": 9,
+      "name": "Surfing on the internet"
+    },
+    {
+      "id": 10,
+      "name": "Meditating"
+    },
+    {
+      "id": 11,
+      "name": "Attending a concert"
+    },
+    {
+      "id": 12,
+      "name": "Scuba diving"
+    }
+  ];
+
+  const randomActivities = activities.sort(() => 0.5 - Math.random()).slice(0, 5);
+
+  // Generate buttons for each activity
+  randomActivities.forEach(activity => {
+    const button = document.createElement("button");
+    button.classList.add("button", "is-rounded", "is-primary");
+    button.textContent = activity.name;
+    button.addEventListener("click", () => {
+      window.location.replace(`activity/${activity.id}`);
     });
+    document.getElementById("RandomButtons").appendChild(button);
   });
+});
+
+//addEventListener for menu dropdown selections
+document.addEventListener("DOMContentLoaded", function (event) {
+  var button = document.getElementById("logout");
+
+  button.addEventListener("click", function () {
+    document.location.href = 'http://localhost:3001/login';
+  });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var button = document.getElementById("Homepage");
+
+  button.addEventListener("click", function () {
+    document.location.href = 'http://localhost:3001/';
+  });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var button = document.getElementById("Find an Activity");
+
+  button.addEventListener("click", function () {
+    document.location.href = 'http://localhost:3001/FindAnActivity';
+  });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var button = document.getElementById("Explore a Strain");
+
+  button.addEventListener("click", function () {
+    document.location.href = 'http://localhost:3001/strains';
+  });
+})
--- public/js/findStrain.js
+++ public/js/findStrain.js
@@ -1,28 +1,85 @@
 
-// get search bar element
-const searchInput = document.getElementById("searchInput");
+// // get search bar element
+// const searchInput = document.getElementById("searchInput");
 
-// store name elements in array-like object
-const namesFromDOM = document.getElementsByClassName("name");
+// // store name elements in array-like object
+// const namesFromDOM = document.getElementsByClassName("name");
 
-// listen for user events
-searchInput.addEventListener("keyup", (event) => {
-    const { value } = event.target;
-    
-    // get user search input converted to lowercase
-    const searchQuery = value.toLowerCase();
-    
-    for (const nameElement of namesFromDOM) {
-        // store name text and convert to lowercase
-        let name = nameElement.textContent.toLowerCase();
-        
-        // compare current name to search input
-        if (name.includes(searchQuery)) {
-            // found name matching search, display it
-            nameElement.style.display = "block";
-        } else {
-            // no match, don't display name
-            nameElement.style.display = "none";
-        }
-    }
-});
+// // listen for user events
+// searchInput.addEventListener("keyup", (event) => {
+//     const { value } = event.target;
+
+//     // get user search input converted to lowercase
+//     const searchQuery = value.toLowerCase();
+
+//     for (const nameElement of namesFromDOM) {
+//         // store name text and convert to lowercase
+//         let name = nameElement.textContent.toLowerCase();
+
+//         // compare current name to search input
+//         if (name.includes(searchQuery)) {
+//             // found name matching search, display it
+//             nameElement.style.display = "block";
+//         } else {
+//             // no match, don't display name
+//             nameElement.style.display = "none";
+//         }
+//     }
+// });
+
+
+
+//addEventListener for Strain types
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var button = document.getElementById("sativa-btn");
+
+    button.addEventListener("click", function () {
+        document.location.href = 'http://localhost:3001/strain/sativa';
+    });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var button = document.getElementById("indica-btn");
+
+    button.addEventListener("click", function () {
+        document.location.href = 'http://localhost:3001/strain/indica';
+    });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var button = document.getElementById("hybrid-btn");
+
+    button.addEventListener("click", function () {
+        document.location.href = 'http://localhost:3001/strain/hybrid';
+    });
+})
+
+//addEventListener for menu dropdown selections
+document.addEventListener("DOMContentLoaded", function (event) {
+    var button = document.getElementById("Homepage");
+
+    button.addEventListener("click", function () {
+        document.location.href = 'http://localhost:3001/';
+    });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var button = document.getElementById("Find an Activity");
+
+    button.addEventListener("click", function () {
+        document.location.href = 'http://localhost:3001/FindAnActivity';
+    });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var button = document.getElementById("Explore a Strain");
+
+    button.addEventListener("click", function () {
+        document.location.href = 'http://localhost:3001/strains';
+    });
+})
