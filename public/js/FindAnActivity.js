c734a45e1a330aac620254efc3fa1ba1a398c4a9
Aaron <awitthohn@yahoo.com>
2023-05-07 14:34:09 -0400

    readded event listeners

--- public/js/FindAnActivity.js
+++ public/js/FindAnActivity.js
@@ -64,3 +64,39 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("RandomButtons").appendChild(button);
   });
 });
+
+//addEventListener for menu dropdown selections
+document.addEventListener("DOMContentLoaded", function (event) {
+  var button = document.getElementById("logout");
+
+  button.addEventListener("click", function () {
+    document.location.href = 'http://localhost:3001/login';
+  });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var button = document.getElementById("Homepage");
+
+  button.addEventListener("click", function () {
+    document.location.href = 'http://localhost:3001/';
+  });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var button = document.getElementById("Find an Activity");
+
+  button.addEventListener("click", function () {
+    document.location.href = 'http://localhost:3001/FindAnActivity';
+  });
+})
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var button = document.getElementById("Explore a Strain");
+
+  button.addEventListener("click", function () {
+    document.location.href = 'http://localhost:3001/strains';
+  });
+})
