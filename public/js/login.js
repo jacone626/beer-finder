61a48ccc2fc456cb3d3424c96f67f428ac9a467f
Aaron <awitthohn@yahoo.com>
2023-05-08 15:33:14 -0400

    TS login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,10 @@
-select.addEventListener("click", function() {
-  document.querySelector(".select-dropdown").classList.toggle("show");
-});
-options.forEach(function(option) {
-  option.addEventListener("click", function() {
-    var value = option.dataset.value;
-    select.value = value;
-    document.querySelector(".select-dropdown").classList.remove("show");
-  });
-});
 const loginFormHandler = async (event) => {
   event.preventDefault();
+
   // Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
+
   if (email && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
@@ -20,6 +12,7 @@ const loginFormHandler = async (event) => {
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
+
     if (response.ok) {
       // If successful, redirect the browser to the profile page
       document.location.replace('/');
@@ -28,17 +21,21 @@ const loginFormHandler = async (event) => {
     }
   }
 };
+
 const signupFormHandler = async (event) => {
   event.preventDefault();
+
   const name = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
+
   if (name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({ name, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
+
     if (response.ok) {
       document.location.replace('/');
     } else {
@@ -46,28 +43,39 @@ const signupFormHandler = async (event) => {
     }
   }
 };
+
 //addEventListener for menu dropdown selections
 document.addEventListener("DOMContentLoaded", function (event) {
   var button = document.getElementById("Homepage");
+
   button.addEventListener("click", function () {
     document.location.href = window.location.origin;
   });
 })
+
+
 document.addEventListener("DOMContentLoaded", function (event) {
   var button = document.getElementById("Find an Activity");
+
   button.addEventListener("click", function () {
       document.location.href = window.location.origin + '/FindAnActivity';
   });
 })
+
+
 document.addEventListener("DOMContentLoaded", function (event) {
   var button = document.getElementById("Explore a Strain");
+
   button.addEventListener("click", function () {
     document.location.href = window.location.origin + '/strains';
   });
 })
+
+
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
